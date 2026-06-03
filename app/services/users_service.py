08970dc4078fb767c5d261621c3441b3e3b46184
Timur <timur242008@mail.ru>
2026-06-03 23:49:44 +0300

    dada

--- app/services/users_service.py
+++ app/services/users_service.py
@@ -7,3 +7,4 @@ from app.database import models
 def get_by_username(*, session: Session, username: str) -> models.User | None:
 	query = select(models.User).where(models.User.username == username)
 	return session.scalar(query)
+
